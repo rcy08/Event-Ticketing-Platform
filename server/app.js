12606e33f1c748d23c5764f7c80ee71aa2235aaa
Chinmay <rajchinmay08@gmail.com>
2023-09-10 23:24:46 +0530

    updating port

--- server/app.js
+++ server/app.js
@@ -20,11 +20,9 @@ app.use(
     })
   );
 
-const PORT = process.env.PORT || 5000;
-
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
-        app.listen(PORT, () => {
+        app.listen(process.env.PORT, () => {
             console.log(`Connected to db and listening on port ${PORT}`);
         });
     })
