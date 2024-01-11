c622f65e44b78a53e02f730c1d3fe7320b917f7b
Chinmay <rajchinmay08@gmail.com>
2024-01-11 22:29:12 +0530

    minor changes

--- server/utils/initializeFirebase.js
+++ server/utils/initializeFirebase.js
@@ -1,5 +1,6 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../ticketvibe-firebase-adminsdk-qys38-42702b043e.json');
+const config = require('../ticketvibe-firebase-adminsdk-qys38-42702b043e');
+const serviceAccount = config;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
